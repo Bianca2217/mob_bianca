24b0b98cee29d4baa639a7936963d35be3c83ef6
Bianca 🕊️ <bianca.arconti.lima@escola.pr.gov.br>
2026-09-22 21:13:43 +0000

    exercicio 4 feito

--- app-laboratorio/App.js
+++ app-laboratorio/App.js
@@ -1,17 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-const aluno = {
+const eu = {
 nome: "Bianca",
 idade: 16,
-cidade: "Cascavel"
+cidade: "Cascavel",
+nota1: 9.5,
+nota2: 9.0
 };
+
+function calcularMedia(eu) {
+return (eu.nota1 + eu.nota2) / 2;
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{aluno.nome}</Text>
-      <Text>{aluno.idade}</Text>
-      <Text>{aluno.cidade}</Text>
+      <Text>{eu.nome}</Text>
+      <Text>{eu.idade}</Text>
+      <Text>{eu.cidade}</Text>
+      <Text>{eu.nota1}</Text>
+      <Text>{eu.nota2}</Text>
+      <Text>{calcularMedia(eu)}</Text>
     </View>
   );
 }
